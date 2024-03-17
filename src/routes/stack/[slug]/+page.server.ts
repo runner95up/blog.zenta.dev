b41d03e89bea3821d941c6fd03e69ab99143adbb
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-03-18 00:44:41 +0700

    chore: 🧹 bump adapter to auto

--- src/routes/stack/[slug]/+page.server.ts
+++ src/routes/stack/[slug]/+page.server.ts
@@ -1,9 +1,8 @@
 import { BYPASS_TOKEN } from "$env/static/private";
 import { prisma } from "@/lib/server/prisma";
-import { type Config } from "@sveltejs/adapter-vercel";
 import type { PageServerLoad } from "./$types";
 
-export const config: Config = {
+export const config = {
   isr: {
     expiration: 60,
     bypassToken: BYPASS_TOKEN,
