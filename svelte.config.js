65f1783159c60984bff2e9daee9de5c84e1380b0
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-03-18 01:23:24 +0700

    experimental: 🧪 using verceladapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
   preprocess: [vitePreprocess({})],
 
   kit: {
-    adapter: adapter({
-      fallback: "404.html",
-      precompress: true,
-    }),
+    adapter: adapter(),
     alias: {
       "@/*": "./src/*",
       "$l/*": "./src/lib/*",
