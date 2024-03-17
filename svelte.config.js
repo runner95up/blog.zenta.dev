945b7ca5ca01577c08ebf6eb6a4c4d15d5ca5744
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-03-18 01:02:14 +0700

    experimental: 🧪 using static site generation adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,10 @@ const config = {
   preprocess: [vitePreprocess({})],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: "404.html",
+      precompress: true,
+    }),
     alias: {
       "@/*": "./src/*",
       "$l/*": "./src/lib/*",
