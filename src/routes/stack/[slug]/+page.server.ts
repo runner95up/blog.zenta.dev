04210efc8cf970d63c2e7a30a3152e82c47d8e70
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-03-18 01:39:30 +0700

    experimental: 🧪 update adpater to auto

--- src/routes/stack/[slug]/+page.server.ts
+++ src/routes/stack/[slug]/+page.server.ts
@@ -1,8 +1,9 @@
 import { BYPASS_TOKEN } from "$env/static/private";
 import { prisma } from "@/lib/server/prisma";
+import type { Config } from "@sveltejs/adapter-vercel";
 import type { PageServerLoad } from "./$types";
 
-export const config = {
+export const config: Config = {
   isr: {
     expiration: 60,
     bypassToken: BYPASS_TOKEN,
