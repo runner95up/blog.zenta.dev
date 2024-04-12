a0cc093a70230d36fbdec60158e82d8f6c56e307
Rahmat Hidayatullah <bokirsianpar95@gmail.com>
2024-04-12 13:43:36 +0700

    chore: 🧹 remove commited .env

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,15 +1,12 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Prisma, PrismaClient } from "@prisma/client";
-import { getServerSession } from "next-auth/next";
+import { PrismaClient } from "@prisma/client";
 import { Pool } from "pg";
-import { options } from "./auth";
 
 const dbUrl = process.env.DATABASE_URL;
 
 const pool = new Pool({ connectionString: dbUrl });
 const adapter = new PrismaPg(pool);
 
-const prisma = new PrismaClient({ adapter }) ;
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
- 
